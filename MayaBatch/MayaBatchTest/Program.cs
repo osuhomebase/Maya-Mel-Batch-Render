57a25a25e21346c8f0171977592df0bb9c29dd15
osuhomebase <mbaron999@gmail.com>
2015-09-08 23:32:13 -0400

    added test project.

---
+++ MayaBatch/MayaBatchTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MayaBatch;
+
+
+namespace MayaBatchTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderBatch testBatch = new RenderBatch
+            {
+                binDirectory = "C:\\Program Files\\AutoDesk\\Maya2015\\bin",
+                startFrame = 100,
+                endFrame = 120,
+                cameraName = "camera1",
+                outputFileDirectory = "C:\\MayaOutput",
+                outputFilePrefix = "testBatch",
+                fileDirectory = "C:\\Users\\mrb14\\Downloads",
+                fileName = "AA_test01.mb"
+
+            };
+
+            testBatch.Render();
+            Console.ReadLine();
+                
+        }
+    }
+}
